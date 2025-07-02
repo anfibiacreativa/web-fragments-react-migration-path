3584f2a6c40c196697df2d1118dbfae8de73e94f
Natalia Venditto <nataliafrontend@gmail.com>
2025-07-02 11:26:06 +0200

    fix: rename file to avoid case issues

--- packages/host-app/client/src/components/Banner.tsx
+++ packages/host-app/client/src/components/Banner.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import styles from '../styles/CountDownBanner.module.css';
+import styles from '../styles/Banner.module.css';
 
 const CountdownBanner: React.FC = () => {
   const [now, setNow] = useState<Date | null>(null); // Start with null to ensure it doesn't render SSR time
